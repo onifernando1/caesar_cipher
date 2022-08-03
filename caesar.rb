c6101a1d45fc918bacf60d94ae4689abd04521d0
onifernando1 <onifernando1@gmail.com>
2022-08-03 23:24:30 +0100

    Fixed wrap

--- caesar.rb
+++ caesar.rb
@@ -1,25 +1,43 @@
 # frozen_string_literal: true
 
-def caesar_cipher(string, shift)
-  lower = string.downcase
-  split = lower.split('')
-  split_ascii = split.map(&:ord)
-  p split_ascii
-  ascii_cypher = split_ascii.map do |number|
-    if number == 32 || number <= 98 || number >= 123
-      number
-    elsif (number + shift) <= 122 && (number + shift) >= 97
-      number + shift
-    elsif (number + shift) >= 123
-      number + shift - 26
-    elsif (number + shift) <= 98
-      (number + shift) + 27
-    end
-  end
-  p ascii_cypher
-  split_cypher = ascii_cypher.map(&:chr)
-  cypher = split_cypher.join('')
-  p cypher
-end
+class Caesar
 
-caesar_cipher('What a string!', 5)
+  def caesar_cipher(string, shift)
+    lower = string.downcase
+    split = lower.split('')
+    split_ascii = split.map(&:ord)
+    p split_ascii
+    ascii_cypher = split_ascii.map do |number|
+      if number == 32 || number < 97 || number >= 123
+        number 
+      elsif (number + shift) <= 122 && (number + shift) >= 97
+        number + shift
+      elsif number + shift > 122 
+        new_shift = number + shift 
+        until new_shift <= 122 
+          new_shift -= 26
+          
+        end 
+        new_shift 
+      elsif number + shift < 97 
+        new_shift = number + shift 
+        until new_shift <= 97
+          new_shift +=26
+        end
+        new_shift 
+    end
+  end 
+
+    
+    split_cypher = ascii_cypher.map(&:chr)
+    cypher = split_cypher.join('')
+    p cypher
+    cypher
+
+  end
+
+end 
+
+caesar = Caesar.new()
+
+caesar.caesar_cipher("h",35)
